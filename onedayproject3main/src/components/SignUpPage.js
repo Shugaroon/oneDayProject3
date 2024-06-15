c1d255fcb06b53741848dbfd6d17a115200aa692
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-06-16 00:23:04 +0900

    change

--- onedayproject3main/src/components/SignUpPage.js
+++ onedayproject3main/src/components/SignUpPage.js
@@ -7,7 +7,6 @@ export default function App() {
       <div className="white-container">
         <div className="signUp-container">
           <div className="SignUp-form-wrapper">
-            <p></p>
             <SignUpForm />
           </div>
         </div>
