8b99aed502818577c29e101b1b94063e921e6cd1
shircorgi <gusrud21234@gmail.com>
2024-06-16 21:31:01 +0900

    out-of-service modify

--- onedayproject3main/src/components/OutOfService.js
+++ onedayproject3main/src/components/OutOfService.js
@@ -5,7 +5,7 @@ function OutOfService() {
   return (
     <div className="out-service-container">
       <div className="out-service-box">
-        <div className="out-service-logo">Logo</div>
+        <div className="out-service-logo">TEAM KOK</div>
         <h3>⛑️ 점검중...</h3>
         <h2>현재 서비스 점검 중입니다.</h2>
         <h5>더 나은 서비스로 찾아뵙겠습니다.</h5>
